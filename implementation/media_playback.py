94fb7708eb40c9fcedc1e2298272d6ffd2690e58
Coldjack007 <69972794+Coldjack007@users.noreply.github.com>
2025-07-19 16:45:36 +0200

    Piano now plays sounds properly. Changed the soundfont.

--- implementation/media_playback.py
+++ implementation/media_playback.py
@@ -9,10 +9,11 @@ class MediaPlayback:
 
     def __init__(self, piano: Piano):
         self.piano = piano
+        self.active_notes = set()
         self.fs = fluidsynth.Synth()
-        self.fs.start()
+        self.fs.start(driver="dsound", midi_driver=None)
 
-        sfid = self.fs.sfload("FluidR3_GM.sf2")
+        sfid = self.fs.sfload("implementation/steinway_concert_piano.sf2")
         self.fs.program_select(0, sfid, 0, 0)
 
     def update(self, dt: float, frame: np.ndarray) -> None:
@@ -25,16 +26,20 @@ class MediaPlayback:
         for note in notes:
             octave = note.octave + 1
             key = note.key
-            midi_note = 12 * octave + {
+            midi_note = 36 + 12 * octave + {
                 'C': 0, 'C#': 1, 'D': 2, 'D#': 3,
                 'E': 4, 'F': 5, 'F#': 6, 'G': 7,
                 'G#': 8, 'A': 9, 'A#': 10, 'H': 11
             }[key]
             
             if note.last_activation is not None:
-                self.fs.noteon(1, midi_note, 30)
+                if midi_note not in self.active_notes:
+                    self.fs.noteon(0, midi_note, 30)
+                    self.active_notes.add(midi_note)
             elif note.last_activation is None:
-                self.fs.noteoff(1, midi_note)
+                if midi_note in self.active_notes:
+                    self.fs.noteoff(0, midi_note)
+                    self.active_notes.remove(midi_note)
 
     def visualize_keys(self, frame: np.ndarray, notes: list[Note]) -> None:
         """Visualizes the pressed keys in a cv2 window."""
